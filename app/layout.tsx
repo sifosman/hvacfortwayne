0b936256c8090bf762df4b88625ef5717693341b
Mohamed Osman <MOsman2@sars.gov.za>
2026-05-25 08:47:33 +0200

    Add Google Analytics 4 tracking tag (gtag.js)

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,6 +70,18 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-HM7GLSJ9FQ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HM7GLSJ9FQ');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
